a8e1191bc0e6a4fc4f6c9228c6d6b42ca90b05c3
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-10 22:54:43 +0200

    display label in show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,7 +8,7 @@
 
         <div class="container">
 
-            <h1>{{$project->id}}: {{$project->title}}</h1>
+            <h1>{{$project->id}}: {{$project->title}} <span class="badge text-bg-secondary">{{$project->type->label ?? ''}}</span></h1>
 
             <h2 class="mt-4">Description:</h2>
             <div>
